a0ab24e9ad3828508c940a0b6afe7c2f368da0fa
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2025-01-02 15:18:13 +0530

    example

---
+++ ternaryExample.jsx
@@ -0,0 +1,9 @@
+// Use of ternary operators - 
+let age = 21;
+var name = age > 10 ? "Sujal" : "Rose";
+console.log(name);
+
+var age = 21;
+const myComponent = () => {
+    return age > 10 ? <div> Sujal </div> : <div> Rose </div>;
+}
