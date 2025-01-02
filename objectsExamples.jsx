6fa14aa693d3fc85113d733a2beb4805931aaa23
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2025-01-02 23:36:56 +0530

    example

---
+++ objectsExamples.jsx
@@ -0,0 +1,11 @@
+height = 111;
+const person = {
+    name: "Pujal",
+    age: 20,
+    height,       //we can use global/local variables
+};
+
+// const {name, age, height} = person;
+// person.height = 21;
+
+console.log(person.height);
