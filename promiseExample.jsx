0248185e3cec190429ccb7f6cf82aa616a78a62e
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2025-01-03 13:34:38 +0530

    example

--- promiseExample.jsx
+++ promiseExample.jsx
@@ -1,15 +1,17 @@
-const newEvent = new Promise((resolve, reject) => {            
+// The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+const newEvent = new Promise((resolve, reject) => {            // It kind of works like try and catch 
     let name = "Pujal";
     if (name == "Pujal"){
-        resolve(name);
+        resolve(name);                            // If a condition is met, resolve the promise
     }else{
-        reject("Name was not Pujal, try again");
+        reject("Name was not Pujal, try again");        // Else, we reject it
     }                            
 });
 
 newEvent
-.then((name) => {
-    console.log(name);
+.then((name) => {                // We handle the accepted or rejected output here and further work on it however we like to
+    console.log(name);    
 })
 .catch((err) => {
     console.log(err);
